b7e73ed41c6e8716d05f9b7b43e99c0a5d7a4b5d
Brian Mock <brian@mockbrian.com>
2020-07-15 17:20:43 -0700

    docs

--- src/parse-result.ts
+++ src/parse-result.ts
@@ -1,30 +1,50 @@
 import { SourceLocation } from "./source-location";
 
+/**
+ * Represents the result of calling a parse action.
+ *
+ * Either a ParseOK<A> or a ParseFail. To check if the parse result is OK, use
+ * the `isOK()` method.
+ */
 export type ParseResult<A> = ParseOK<A> | ParseFail;
 
+/**
+ * Represents a successful parse action result.
+ *
+ * @typeParam A the type of the value parsed
+ */
 export class ParseOK<A> {
+  /** The parsed value */
   value: A;
 
+  /** @ignore */
   constructor(value: A) {
     this.value = value;
   }
 
+  /** Returns true */
   isOK(): this is ParseOK<A> {
     return true;
   }
 }
 
+/**
+ * Represents a failed parse action result, where it failed, and what types of
+ * values were expected at the point of failure.
+ */
 export class ParseFail {
-  // TODO: Possibly yoink the properties off location and put them straight on
-  // this class so it's easier to look at the object?
+  /** The input location where the parse failed */
   location: SourceLocation;
+  /** List of expected values at the location the parse failed */
   expected: string[];
 
+  /** @ignore */
   constructor(location: SourceLocation, expected: string[]) {
     this.location = location;
     this.expected = expected;
   }
 
+  /** Returns false */
   isOK<A>(): this is ParseOK<A> {
     return false;
   }
