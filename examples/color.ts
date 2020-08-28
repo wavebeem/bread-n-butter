0dd4f7688cff738369be8093b1b394e8d66047fb
Brian Mock <brian@mockbrian.com>
2020-08-28 07:47:01 -0700

    Enhances color.ts example (#12)

--- examples/color.ts
+++ examples/color.ts
@@ -8,68 +8,53 @@ class Color {
     public b: number,
     public a: number = 1.0
   ) {}
-
-  toString() {
-    return (
-      `r:${this.r} g:${this.g} b:${this.b}` + (this.a ? ` alpha:${this.a}` : "")
-    );
-  }
 }
 
-// primitives
+// Primitives
+const ws = bnb.match(/\s*/);
 const sharp = bnb.text("#");
+const hexDigit = bnb.match(/[0-9a-fA-F]/);
+const hex1 = hexDigit.map((d) => parseInt(d + d, 16));
+const hex2 = hexDigit.and(hexDigit).map(([d1, d2]) => parseInt(d1 + d2, 16));
+const number = bnb.match(/[0-9]+(\.[0-9]*)?/).map(Number);
 
-const hexval = bnb.match(/[0-9a-fA-F]/);
+// Color types
+const hexColorShort = sharp
+  .next(bnb.all(hex1, hex1, hex1))
+  .map(([r, g, b]) => new Color(r, g, b));
 
-const num = bnb.match(/[0-9.]+/);
-
-// parsers
-const hexColor1 = sharp
-  .next(bnb.all(hexval, hexval, hexval))
-  .map(
-    ([r, g, b]) =>
-      new Color(parseInt(r + r, 16), parseInt(g + g, 16), parseInt(b + b, 16))
-  );
-const hexColor2 = sharp
-  .next(
-    bnb.all(
-      bnb.all(hexval, hexval).map((strs) => strs.join("")),
-      bnb.all(hexval, hexval).map((strs) => strs.join("")),
-      bnb.all(hexval, hexval).map((strs) => strs.join(""))
-    )
-  )
-  .map(
-    ([r, g, b]) => new Color(parseInt(r, 16), parseInt(g, 16), parseInt(b, 16))
-  );
+const hexColorLong = sharp
+  .next(bnb.all(hex2, hex2, hex2))
+  .map(([r, g, b]) => new Color(r, g, b));
 
 const rgbColor = bnb
   .text("rgb(")
-  .next(num.sepBy1(bnb.text(",")))
+  .next(number.trim(ws).sepBy1(bnb.text(",")).trim(ws))
   .skip(bnb.text(")"))
-  .map(([r, g, b]) => new Color(parseInt(r), parseInt(g), parseInt(b)));
+  .map(([r, g, b]) => new Color(r, g, b));
 
 const rgbaColor = bnb
   .text("rgba(")
-  .next(num.sepBy1(bnb.text(",")))
+  .next(number.trim(ws).sepBy1(bnb.text(",")).trim(ws))
   .skip(bnb.text(")"))
-  .map(
-    ([r, g, b, a]) =>
-      new Color(parseInt(r), parseInt(g), parseInt(b), parseFloat(a))
-  );
+  .map(([r, g, b, a]) => new Color(r, g, b, a));
 
-// Note that hexColor2 must be placed before hexColor1
-const parsers = bnb.choice(hexColor2, hexColor1, rgbColor, rgbaColor);
+// Note that hexColorLong must be placed before hexColorShort. `bnb.choice`
+// picks the first parser that matches, and `.parse` and `.tryParse` error if
+// your parser does not consume the entire input. If you put `hexColorShort`
+// first then the last 3 hex digits would be left over at the end when parsing a
+// 6-digit hex color, causing an error.
+const cssColor = bnb.choice(hexColorLong, hexColorShort, rgbColor, rgbaColor);
 
 const examples = [
   "#fff",
   "#000000",
   "#238945",
-  "rgb(47,0,0)",
-  "rgba(0,0,0,0.5)",
+  "rgb(47, 0, 0)",
+  "rgba(0, 0, 0, 0.5)",
 ];
 
-const astList = examples.map((v) => parsers.tryParse(v));
-
-prettyPrint(astList);
-
-astList.map((x) => prettyPrint(x.toString()));
+for (const colorText of examples) {
+  const color = cssColor.tryParse(colorText);
+  prettyPrint(color);
+}
