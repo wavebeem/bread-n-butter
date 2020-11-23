7858a4a1ce890032d946b02e8455271ea7830f29
Brian Mock <brian@mockbrian.com>
2020-11-22 21:40:17 -0800

    py

---
+++ __test__/example-python-ish.test.ts
@@ -0,0 +1,35 @@
+import Python from "../examples/python-ish";
+import { snapTest } from "./util";
+
+test("py complex", () => {
+  snapTest(
+    Python,
+    `\
+block:
+  alpha
+  bravo
+  block:\r
+          charlie
+          delta\r
+          echo
+          block:
+           foxtrot
+  golf\
+`
+  );
+});
+
+test("py simple", () => {
+  snapTest(
+    Python,
+    `\
+block:
+  alpha
+  bravo
+`
+  );
+});
+
+test("py even simpler", () => {
+  snapTest(Python, `alpha`);
+});
