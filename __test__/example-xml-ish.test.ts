424fd1b8e1866b64ead0b17dce688fc058e42e2b
Brian Mock <brian@mockbrian.com>
2020-12-06 10:37:48 -0800

    adds xml parser example

--- __test__/example-xml-ish.test.ts
+++ __test__/example-xml-ish.test.ts
@@ -1,50 +1,37 @@
 import XML from "../examples/xml-ish";
-// import { snapTest } from "./util";
+import { snapTest } from "./util";
 
-// TODO: Better tests
-test("xml", () => {
-  // expect(XML.tryParse(`<a />`)).toEqual({
-  //   name: "a",
-  //   attributes: {},
-  //   children: [],
-  // });
+test("xml basic", () => {
   expect(XML.tryParse(`<a key="val" />`)).toEqual({
     name: "a",
     attributes: { key: "val" },
     children: [],
   });
-  // expect(XML.tryParse(`<div />`)).toEqual({
-  //   name: "div",
-  //   attributes: {},
-  //   children: [],
-  // });
-  // expect(XML.tryParse(`<div></div>`)).toEqual({
-  //   name: "div",
-  //   attributes: {},
-  //   children: [],
-  // });
-  //   expect(
-  //     XML.tryParse(`\
-  // <div>\
-  //   &amp; i'm the &quot;bad guy&quot;\
-  //   who&apos;s with me? &lt;3\
-  // </div>\
-  // `)
-  //   ).toEqual({
-  //     name: "a",
-  //     attributes: { key: "val" },
-  //     children: [],
-  //   });
+  expect(XML.tryParse(`<a key="val">foo<b /></a>`)).toEqual({
+    name: "a",
+    attributes: { key: "val" },
+    children: ["foo", { name: "b", attributes: {}, children: [] }],
+  });
 });
 
-// test("xml", () => {
-//   snapTest(
-//     XML,
-//     `\
-// <dict>
-//     <item key="a">alpha</item>
-//     <item key="empty" />
-// </dict>
-// `
-//   );
-// });
+test("xml large example", () => {
+  snapTest(
+    XML,
+    `
+    <Map>
+      <Entry key="string-a"><String>alpha</String></Entry>
+      <Entry key="string-b"><String>bravo</String></Entry>
+      <Entry key="string-c"><String>charlie</String></Entry>
+      <Entry key="list">
+        <List>
+          <String>a</String>
+          <String>b</String>
+          <String>c</String>
+        </List>
+      </Entry>
+      <Entry key="null"><Null /></Entry>
+      <Entry key="empty" />
+    </Map>
+    `
+  );
+});
--- examples/xml-ish.ts
+++ examples/xml-ish.ts
@@ -1,98 +1,73 @@
 import * as bnb from "../src/bread-n-butter";
 
-// Like Object.fromEntries
-function fromEntries(entries: [string, string][]) {
-  const ret: Record<string, string> = {};
-  for (const [key, val] of entries) {
-    ret[key] = val;
-  }
-  return ret;
-}
-
-interface XMLElement {
+export interface XMLElement {
   name: string;
   attributes: Record<string, string>;
-  children: (XMLElement | string)[];
+  children: (string | XMLElement)[];
 }
 
-const ws0 = bnb.match(/\s*/m); // whitespace, 0+
-const ws1 = bnb.match(/\s+/m); // whitespace, 1+
+// Mandatory whitespace
+const W1 = bnb.match(/\s+/);
 
-const XML: bnb.Parser<XMLElement> = bnb.lazy(() => {
-  return bnb.choice(elementWithoutChildren);
-});
+// Optional whitespace
+const W0 = W1.or(bnb.ok(""));
 
-// Tag or attribute name
-const symbol = bnb.match(/[a-zA-Z_][a-zA-Z0-9_-]*/);
+const Word = bnb.match(/[a-zA-Z]+/);
 
-// XML entity
-const strEscape = bnb.choice(
-  bnb.text("&quot;").map(() => '"'),
-  bnb.text("&apos;").map(() => "'"),
-  bnb.text("&amp;").map(() => "&"),
-  bnb.text("&lt;").map(() => "<"),
-  bnb.text("&gt;").map(() => ">")
-);
+// Proper XML attributes would support XML entities (e.g. `&amp;`)
+const AttributeValue = bnb.match(/[^"]+/).trim(bnb.text('"'));
 
-// One or more characters that aren't `"` or `&`
-const strChunk = bnb.match(/[^"&]+/);
+// `name="value"`.
+const Attribute = bnb.all(Word.skip(bnb.text("=")), AttributeValue);
 
-const attrString = bnb
-  .choice(strEscape, strChunk)
-  .repeat()
-  .map((parts) => parts.join(""))
-  .trim(bnb.text('"'));
-
-const oneAttr = bnb.all(
-  ws1.next(symbol).skip(bnb.text("=").trim(ws0)),
-  attrString
-);
-
-export const attributes = oneAttr.repeat().map((attrs) => {
-  return fromEntries(attrs);
-});
-
-// XML entity
-const xmlChildEscape = bnb.choice(
-  bnb.text("&quot;").map(() => '"'),
-  bnb.text("&apos;").map(() => "'"),
-  bnb.text("&amp;").map(() => "&"),
-  bnb.text("&lt;").map(() => "<"),
-  bnb.text("&gt;").map(() => ">")
-);
-
-// One or more characters that aren't `"` or `&`
-const xmlChildChunk = bnb.match(/[^"&<]+/);
-
-export const xmlChildString = bnb
-  .choice(xmlChildEscape, xmlChildChunk)
-  .repeat(1)
-  .map((parts) => parts.join(""));
-
-export const xmlChild = bnb.lazy(() => bnb.choice(xmlChildString));
-
-const elementWithChildren = bnb
-  .all(bnb.text("<"), symbol, attributes, ws0, bnb.text(">"))
-  .chain(([, tagName, attrs]) => {
-    return xmlChild.repeat().chain((children) => {
-      return bnb.text(`<${tagName}/>`).map<XMLElement>(() => {
-        return {
-          name: tagName,
-          attributes: attrs,
-          children: children,
-        };
-      });
-    });
+// Both types of opening tag (`<x>` and `<x/>`) contain a name followed by
+// optional attributes
+const OpeningTagInsides = bnb
+  .all(Word, W0.next(Attribute.sepBy(W1)).or(bnb.ok([])))
+  .map(([name, attrList]) => {
+    const attributes: Record<string, string> = {};
+    for (const [key, value] of attrList) {
+      attributes[key] = value;
+    }
+    return { name, attributes };
   });
 
-const elementWithoutChildren = bnb
-  .all(bnb.text("<"), symbol, ws1, attributes, ws0, bnb.text("/>"))
-  .map<XMLElement>(([, tagName, , attrs]) => {
-    return {
-      name: tagName,
-      attributes: attrs,
-      children: [],
-    };
-  });
+// `<tag>`
+const OpeningTag = OpeningTagInsides.wrap(
+  bnb.text("<"),
+  W0.next(bnb.text(">"))
+);
+
+// `<tag />`
+const EmptyTag = OpeningTagInsides.wrap(bnb.text("<"), W0.next(bnb.text("/>")));
+
+// - Full elements have an opening and closing tag (e.g. `<a></a>`)
+// - Empty elements just have an empty tag (e.g. `<a/>`).
+const Element: bnb.Parser<XMLElement> = bnb.lazy(() => {
+  return bnb.choice(FullElement, EmptyElement).skip(W0);
+});
+
+// Construct an appropriate output object, and use the parsed tag name to
+// create a closing tag parser on the fly, to check that a matching end tag
+// is found.
+const FullElement = OpeningTag.chain(({ name, attributes }) => {
+  return Children.map((children) => {
+    return { name, attributes, children };
+  }).skip(bnb.text(`</${name}>`));
+});
+
+// Empty elements have it easier; we don't have to handle contents or a
+// possible end tag.
+const EmptyElement = EmptyTag.map(({ name, attributes }) => {
+  return { name, attributes, children: [] };
+});
+
+// Proper XML text content would support XML entities (e.g. &amp;).
+const TextContent = bnb.match(/[^<>]+/);
+
+// An element can contain other elements, or text.
+const Children = bnb.choice(Element, TextContent).repeat();
+
+const XML = Element.trim(W0);
 
 export default XML;
